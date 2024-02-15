4d69b1965a27fa0da189a6bf08b61a10b069f5b0
ilf68 <ilf68@mail.ru>
2024-02-16 00:02:17 +0500

    1.4. Вызов метода с возвращаемым типом. [#167387]

--- src/main/java/ru/job4j/oop/DummyDic.java
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -3,7 +3,7 @@ package ru.job4j.oop;
 public class DummyDic {
     public String engToRus(){
         String eng = "it english";
-        return eng;
+        return eng; /*redundant*/
     }
     public  static void main(String[] args) {
        DummyDic petr = new DummyDic();
