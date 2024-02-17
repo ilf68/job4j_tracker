c68de1b564c41d16462d3a9ed9a5a8bbfe1d164e
ilf68 <ilf68@mail.ru>
2024-02-17 15:22:59 +0500

    Конструктор класса

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+    public Error (boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+    public void printInfo() {
+        System.out.println("Опа-на! ошибочка вроде?");
+        System.out.println("Наличие ошибки: " + active);
+        System.out.println("Статус: " + status);
+        System.out.println("Сообщение: " + message);
+    }
+    public Error(){
+
+    }
+
+    public static void main(String[] args) {
+        Error err1 = new Error();
+        Error err2 = new Error(true, 23, "Неизвестная ошибка");
+        Error err3 = new Error(false, 45, "ошибка номер 45");
+        err1.printInfo();
+        err2.printInfo();
+        err3.printInfo();
+    }
+
+}
