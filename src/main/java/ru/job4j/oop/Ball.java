20270ce005567a1209df4157f515f6e6db263de2
ilf68 <ilf68@mail.ru>
2024-02-17 14:04:12 +0500

    Kolobok, есть вопросы

---
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,12 @@
+package ru.job4j.oop;
+
+public class Ball {
+    public void tryRun(int condition) {
+        if (condition == 1) {
+            System.out.println("И сьела колобка");
+        } else {
+            System.out.println("И сбежал колобок");
+        }
+    }
+}
+/* Здесь не вышло с boolean ????*/
