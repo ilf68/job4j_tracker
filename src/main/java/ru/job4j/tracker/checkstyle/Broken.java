013b9d586a1ac30264235ca840d649431dfc166c
ilf68 <ilf68@mail.ru>
2025-02-16 19:08:10 +0500

    6. Правила Checkstyle [#504981 #532803]

---
+++ src/main/java/ru/job4j/tracker/checkstyle/Broken.java
@@ -0,0 +1,28 @@
+package ru.job4j.tracker.checkstyle;
+
+@SuppressWarnings("checkstyle:NoWhitespaceBefore")
+public class Broken {
+    private int sizeOfEmpty = 10;
+    public String surname;
+    public static final String NEWALUE = "";
+
+    void echo() {
+
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d) {
+    }
+
+    String name;
+
+    Broken() {
+
+    }
+
+}
