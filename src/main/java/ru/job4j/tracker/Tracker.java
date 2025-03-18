81e334475e22f34a67f24ac1b3d55a3acaada173
ilf68 <ilf68@mail.ru>
2025-03-18 08:36:28 +0500

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,11 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+
     }
 
     public Item findById(int id) {
@@ -22,4 +29,17 @@ public class Tracker {
         }
         return rsl;
     }
+    public Item[] findByName(String key) {
+        Item[] result = new Item[items.length];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i].getName().equals(key)) {
+                result[count] = items[i];
+                count++;
+            }
+        }
+        return Arrays.copyOf(result, count);
+    }
+
 }
+
