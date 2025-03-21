e7d08d89eb965cb41cf19a9213b4593bdad6a4f3
ilf68 <ilf68@mail.ru>
2025-03-22 01:32:40 +0500

    8. Что такое валидация? [#246864]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -40,6 +40,7 @@ public class Tracker {
         boolean result = index != -1;
         if (result) {
             items[index] = item;
+            item.setId(id);
         }
         return result;
     }
