f41347a77c2403b800d1e8e965a6d1711e4d252c
ilf68 <ilf68@mail.ru>
2025-04-20 12:56:45 +0500

    0.3. Тестирование исключений с junit [#219366]

---
+++ src/test/java/ru/job4j/ex/FactorialTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.ex;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+public class FactorialTest {
+    @Test
+    public void whenException() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    Factorial.calc(-1);
+                });
+        assertThat(exception.getMessage()).isEqualTo("Number could not be less than 0");
+    }
+    }
